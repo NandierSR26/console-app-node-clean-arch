d0f83a51c088624532fca117d7536d83498162f1
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-03 22:16:59 -0500

    fix: Corrección en el adaptador de "yargs" y corrección de formato del codigo

--- src/config/plugins/args.plugin.ts
+++ src/config/plugins/args.plugin.ts
@@ -1,39 +1,39 @@
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
 
-export const yarg = yargs( process.argv )
-  .option('b', {
-    alias: 'base',
-    type: 'number',
+export const yarg = yargs(hideBin(process.argv))
+  .option("b", {
+    alias: "base",
+    type: "number",
     demandOption: true,
-    describe: 'Multiplication table base'
+    describe: "Multiplication table base",
   })
-  .option('l', {
-    alias: 'limit',
-    type: 'number',
+  .option("l", {
+    alias: "limit",
+    type: "number",
     default: 10,
-    describe: 'Multiplicación table limit'
+    describe: "Multiplicación table limit",
   })
-  .option('s', {
-    alias: 'show',
-    type: 'boolean',
+  .option("s", {
+    alias: "show",
+    type: "boolean",
     default: false,
-    describe: 'Show multiplication table'
+    describe: "Show multiplication table",
   })
-  .option('n', {
-    alias: 'name',
-    type: 'string',
-    default: 'multiplication-table',
-    describe: 'File name'
+  .option("n", {
+    alias: "name",
+    type: "string",
+    default: "multiplication-table",
+    describe: "File name",
   })
-  .option('d', {
-    alias: 'destination',
-    type: 'string',
-    default: 'outputs',
-    describe: 'File destination'
+  .option("d", {
+    alias: "destination",
+    type: "string",
+    default: "outputs",
+    describe: "File destination",
   })
-  .check(( argv, options ) => {
-    if(argv.b < 1) throw "El valor de la base debe ser mayor a cero"
+  .check((argv, options) => {
+    if (argv.b < 1) throw "El valor de la base debe ser mayor a cero";
     return true;
   })
-  .parseSync()
+  .parseSync();
