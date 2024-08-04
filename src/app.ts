89782a470e7055bff32da86388c0f4d8fab79a23
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-08-03 19:29:52 -0500

    feat: Funcionalidad para crear archivo e imprimir tabla de multiplicar

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
-const message: string = 'Hola mundo'
+import { mkdirSync, writeFileSync } from "fs";
 
-console.log(message)
+let outputMessage = "";
+const base = 5;
+const headerMessage = `
+=====================================
+        Tabla del ${base}
+=====================================\n
+`;
+
+outputMessage += headerMessage;
+
+for (let i = 1; i <= 10; i++) {
+  outputMessage += `${base} x ${i} = ${base * i} \n`;
+}
+console.log(outputMessage);
+
+const outputPath = "outputs";
+
+mkdirSync(outputPath, { recursive: true });
+writeFileSync(`${outputPath}/tabla-${base}.txt`, outputMessage);
+
+console.log("File created!");
